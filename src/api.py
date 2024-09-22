ec4556e3914ca862bc15d44f6d5df787ea235193
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-09-22 12:07:23 -0300

    build: agregado .gitignore y formateado codigo python

--- src/api.py
+++ src/api.py
@@ -2,22 +2,12 @@ from fastapi import FastAPI, Depends
 from sqlmodel import Field, Session, SQLModel, select
 from db import init_db, get_session
 
+
 class Hero(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
     secret_name: str
     age: int | None = Field(default=None, index=True)
-
-
-# sqlite_file_name = "database.db"
-# sqlite_url = f"sqlite:///{sqlite_file_name}"
-
-# connect_args = {"check_same_thread": False}
-# engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
-
-
-# def create_db_and_tables():
-#     SQLModel.metadata.create_all(engine)
 
 
 app = FastAPI()
