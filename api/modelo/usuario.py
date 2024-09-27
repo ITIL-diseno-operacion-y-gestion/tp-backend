ca0cd1e37e6c5cd4b16681ad1d5acedd24f01226
andres <andres@MBP-de-andres.fibertel.com.ar>
2024-09-27 17:26:07 -0300

    fixed incremental id

--- api/modelo/usuario.py
+++ api/modelo/usuario.py
@@ -1,10 +1,11 @@
+from typing import Optional
 from sqlmodel import SQLModel, Field
 
 
 class Usuario(SQLModel, table=True):
     __tablename__ = "usuarios"
 
-    id: int | None = Field(default=None, primary_key=True)
+    id: Optional[int] = Field(default=None, primary_key=True)
     nombre: str
     apellido: str
     email: str
