a678c0253d18fa44fb37763c0988ab5abd32318c
andres <andres@MBP-de-andres.fibertel.com.ar>
2024-09-28 12:45:11 -0300

    added delete endpoint

--- api/controladores/configuracion.py
+++ api/controladores/configuracion.py
@@ -11,21 +11,21 @@ router = APIRouter(
 
 @router.get("/articulo/{id}")
 def obtener_articulos(id, session: Session = Depends(get_session)):
-    articulos = session.exec(select(Articulo).where(Articulo.id==id)).first()
+    articulos = session.exec(select(Articulo).where(Articulo.id==id).where(Articulo.esta_activo==True)).first()
     return articulos
 
 @router.get("/articulos")
 def obtener_articulos(nombre: str | None = None, session: Session = Depends(get_session)):
     if nombre:
         print("nombre: ", nombre)
-        return session.exec(select(Articulo).where(Articulo.nombre==nombre)).all()
+        return session.exec(select(Articulo).where(Articulo.nombre==nombre).where(Articulo.esta_activo==True)).all()
     else:
         print("nombre null")
-        return session.exec(select(Articulo)).all()
+        return session.exec(select(Articulo).where(Articulo.esta_activo==True)).all()
 
 @router.patch("/articulo/{id}")
 def actualizar_articulos(id, articuloUpdate: ArticuloUpdate, session: Session = Depends(get_session)):
-    articuloDb = session.exec(select(Articulo).where(Articulo.id==id)).first()
+    articuloDb = session.exec(select(Articulo).where(Articulo.id==id).where(Articulo.esta_activo==True)).first()
     if not articuloDb:
         raise HTTPException(status_code=404, detail="Articulo not found")
     articuloData = articuloUpdate.model_dump(exclude_unset=True)
@@ -38,6 +38,17 @@ def actualizar_articulos(id, articuloUpdate: ArticuloUpdate, session: Session = 
 @router.post("/articulos")
 def crear_articulo(articulo: Articulo, session: Session = Depends(get_session)):
     articulo.esta_activo = True
+    session.add(articulo)
+    session.commit()
+    session.refresh(articulo)
+    return articulo
+
+@router.delete("/articulos/{id}")
+def crear_articulo(id, session: Session = Depends(get_session)):
+    articulo = session.exec(select(Articulo).where(Articulo.id==id)).first()
+    if not articulo:
+        raise HTTPException(status_code=404, detail="Articulo not found")
+    articulo.esta_activo = False
     session.add(articulo)
     session.commit()
     session.refresh(articulo)
