9be36a7fabee151e4d55a4da8daa34ca569880bf
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-10-13 18:14:34 -0300

    fix: al obtener los usuarios existentes, no incluyen las contraseñas

--- api/controladores/usuarios.py
+++ api/controladores/usuarios.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
 from ..db import get_session
-from ..modelo.usuario import Usuario
+from ..modelo.usuario import Usuario, UsuarioPublico, UsuarioForm
 
 router = APIRouter(
     prefix="/usuarios",
@@ -9,15 +9,15 @@ router = APIRouter(
 )
 
 
-@router.get("")
+@router.get("", response_model=list[UsuarioPublico])
 def obtener_usuarios(session: Session = Depends(get_session)):
     usuarios = session.exec(select(Usuario)).all()
     return usuarios
 
 
 @router.post("")
-def crear_usuario(usuario: Usuario, session: Session = Depends(get_session)):
-    usuario.id = None
+def crear_usuario(usuario_form: UsuarioForm, session: Session = Depends(get_session)):
+    usuario = Usuario.model_validate(usuario_form)
     session.add(usuario)
     session.commit()
     session.refresh(usuario)
--- api/modelo/usuario.py
+++ api/modelo/usuario.py
@@ -2,11 +2,24 @@ from typing import Optional
 from sqlmodel import SQLModel, Field
 
 
-class Usuario(SQLModel, table=True):
-    __tablename__ = "usuarios"
-
-    id: Optional[int] = Field(default=None, primary_key=True)
+class UsuarioBase(SQLModel):
     nombre: str
     apellido: str
     email: str
+
+
+class UsuarioForm(UsuarioBase):
     contrasenia: str
+
+
+class UsuarioPublico(SQLModel):
+    id: int
+    nombre: str
+    apellido: str
+    email: str
+
+
+class Usuario(UsuarioForm, table=True):
+    __tablename__ = "usuarios"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
