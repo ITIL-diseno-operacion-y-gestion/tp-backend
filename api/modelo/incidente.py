b368e47878563c9b9097d4ade1d94e546e7913ec
Santiago Ruiz Mazzocato <santimazzo98@gmail.com>
2024-10-14 16:09:08 -0300

    feat: restringidos valores posibles para los atributos forma de notificacion, prioridad y categoria de un incidente

--- api/modelo/incidente.py
+++ api/modelo/incidente.py
@@ -1,16 +1,37 @@
 from typing import Optional
 from sqlmodel import SQLModel, Field
 from datetime import datetime
+from enum import Enum
 
+
+class FormaDeNotificacionDeIncidente(Enum):
+    LLAMADA_TELEFONICA = "llamada telefonica"
+    EMAIL = "email"
+    SMS = "sms"
+    FORMULARIO_WEB = "formulario web"
+    CHAT_EN_VIVO = "chat en vivo"
+
+class PrioridadDeIncidente(Enum):
+    BAJA = "baja"
+    MEDIA = "media"
+    ALTA = "alta"
+
+
+class CategoriaDeIncidente(Enum):
+    DE_SEGURIDAD = "de seguridad"
+    TECNICO = "tecnico"
+    DE_DISPONIBILIDAD = "de disponibilidad"
+    DE_DATOS = "de datos"
+    LEGAL = "legal"
 
 class IncidenteForm(SQLModel):
     id_usuario: int = Field(default=None, foreign_key="usuarios.id")
-    forma_de_notificacion: str
+    forma_de_notificacion: FormaDeNotificacionDeIncidente
     reportador: str
     usuarios_afectados: str
     servicios_afectados: str
-    prioridad: str
-    categoria: str
+    prioridad: PrioridadDeIncidente
+    categoria: CategoriaDeIncidente
     informacion_adicional: str
 
 class Incidente(IncidenteForm, table=True):
