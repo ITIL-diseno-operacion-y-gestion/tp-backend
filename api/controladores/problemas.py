3b77d8a078932c34ac93f796254004edffea93da
andres <andres@MBP-de-andres.fibertel.com.ar>
2024-10-19 20:44:27 -0300

    added patch

--- api/controladores/problemas.py
+++ api/controladores/problemas.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 from ..db import get_session
-from ..modelo.problema import Problema, ProblemaForm
+from ..modelo.problema import Problema, ProblemaForm, ProblemaUpdateForm
 from ..modelo.usuario import Usuario
 
 router = APIRouter(
@@ -30,6 +30,23 @@ def crear_problema(
             detail=f"Usuario con id {problema.id_usuario} no encontrado",
         )
 
+    session.add(problema)
+    session.commit()
+    session.refresh(problema)
+    return problema
+
+@router.patch("/{id}")
+def actualizar_problema(
+    id, problema_form: ProblemaUpdateForm, session: Session = Depends(get_session)
+):
+    print("1111")
+    problema = None
+    try:
+        problema = session.get_one(Problema, id)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="Problema not found")
+    problema_nueva_data = problema_form.model_dump(exclude_unset=True)
+    problema.sqlmodel_update(problema_nueva_data)
     session.add(problema)
     session.commit()
     session.refresh(problema)
