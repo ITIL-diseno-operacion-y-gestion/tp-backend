9023ff0520e666a0c595b4f405f4ce6b249e1d97
andres <andres@MBP-de-andres.fibertel.com.ar>
2024-10-21 16:18:52 -0300

    added login logic

--- api/controladores/usuarios.py
+++ api/controladores/usuarios.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 from ..db import get_session
-from ..modelo.usuario import Usuario, UsuarioPublico, UsuarioForm
+from ..modelo.usuario import Usuario, UsuarioPublico, UsuarioForm, UsuarioLoginForm, UsuarioLoginRespuesta
+import secrets
+import string
 
 router = APIRouter(
     prefix="/usuarios",
@@ -22,3 +24,26 @@ def crear_usuario(usuario_form: UsuarioForm, session: Session = Depends(get_sess
     session.commit()
     session.refresh(usuario)
     return usuario
+
+def generar_random_token(length=32):
+    characters = string.ascii_letters + string.digits
+    token = ''.join(secrets.choice(characters) for _ in range(length))
+    return token
+
+@router.post("/login")
+def login_usuario(usuario_form: UsuarioLoginForm, session: Session = Depends(get_session)):
+    usuario = session.exec(
+        select(Usuario).where(Usuario.email == usuario_form.email).where(Usuario.contrasenia == usuario_form.contrasenia)
+    ).first()
+    if not usuario:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Usuario con email y contraseña enviados no encontrado",
+        )
+    return UsuarioLoginRespuesta(
+        id=usuario.id,
+        nombre=usuario.nombre,
+        apellido=usuario.apellido,
+        email=usuario.email,
+        token=generar_random_token()
+    )
